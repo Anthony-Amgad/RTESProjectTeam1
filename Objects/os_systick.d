a190b715182d250f9dfbe9697ca5dfab245180ac
Anthony-Amgad <19p9880@eng.asu.edu.eg>
2023-05-07 20:30:46 +0300

    Everything but interrupt is buggy for some reason

---
+++ Objects/os_systick.d
@@ -0,0 +1,13 @@
+./objects/os_systick.o: \
+  C:\Users\Anthony\ Amgad\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\10.5.1\CMSIS\RTOS2\FreeRTOS\Source\os_systick.c \
+  C:\Users\Anthony\ Amgad\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\RTOS2\Include\os_tick.h \
+  D:\Programs\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  RTE\_Target_1\RTE_Components.h \
+  C:\Users\Anthony\ Amgad\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\TM4C123.h \
+  C:\Users\Anthony\ Amgad\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\TM4C123GH6PM.h \
+  C:\Users\Anthony\ Amgad\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm4.h \
+  C:\Users\Anthony\ Amgad\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\Anthony\ Amgad\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\Anthony\ Amgad\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Users\Anthony\ Amgad\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\mpu_armv7.h \
+  C:\Users\Anthony\ Amgad\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\system_TM4C123.h
--- main.c
+++ main.c
@@ -1,20 +1,317 @@
-#include "tm4c123gh6pm.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+#include "driverlib/gpio.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/timer.h"
+#include "inc/hw_gpio.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_ints.h"
 #include "FreeRTOS.h"
 #include "task.h"
 #include "FreeRTOSConfig.h"
 #include "semphr.h"
 #include "queue.h"
-#include "DIO.h"
+#include "time.h"
 
+#define mainSW_INTURRUPT_PortA ((IRQn_Type)0)
 
-void vApplicationIdleHook( void ){
-	GPIO_PORTF_DATA_R |= 0x04;	
+xTaskHandle xISRTASK;
+
+xQueueHandle xWindowQueue;
+xSemaphoreHandle xMovingSema;
+xSemaphoreHandle xUpSema;
+xSemaphoreHandle xDownSema;
+xSemaphoreHandle xEmptyQueueSema;
+xSemaphoreHandle xStuckSema;
+xSemaphoreHandle xStuckMutex;
+
+void GPIOA_Handler(){
+	portBASE_TYPE xHigherPrio = pdFALSE;
+	xSemaphoreGiveFromISR(xStuckSema, &xHigherPrio);
+	portEND_SWITCHING_ISR(xHigherPrio);
+	GPIOIntClear(GPIO_PORTA_BASE,GPIO_INT_PIN_2);  
+}
+
+void MSISR(){
+	xSemaphoreTake(xStuckSema,0);
+	while(1){
+		xSemaphoreTake(xStuckSema, portMAX_DELAY);
+		xSemaphoreTake(xStuckMutex, portMAX_DELAY);
+		GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_2,0);
+		GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3,GPIO_PIN_3);
+		vTaskDelay(500/ portTICK_RATE_MS);
+		GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_2,0);
+		GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3,0);
+		xSemaphoreGive(xStuckMutex);
+	}
+}
+
+void UpLimit(){
+	while(1){
+		xSemaphoreTake(xUpSema, portMAX_DELAY);
+		uint32_t cnt = 0;
+		while(GPIOPinRead(GPIO_PORTE_BASE, GPIO_PIN_4) == 0 && cnt++<10000000);
+		if(GPIOPinRead(GPIO_PORTE_BASE, GPIO_PIN_4) == GPIO_PIN_4){
+				GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_2,0);
+				GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3,0);
+		}
+	}
+}
+
+void DownLimit(){
+	while(1){
+		xSemaphoreTake(xDownSema, portMAX_DELAY);
+		uint32_t cnt = 0;
+		while(GPIOPinRead(GPIO_PORTE_BASE, GPIO_PIN_5) == 0 && cnt++<10000000);
+		if(GPIOPinRead(GPIO_PORTE_BASE, GPIO_PIN_5) == GPIO_PIN_5){
+				GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_2,0);
+				GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3,0);
+		}
+	}
+}
+
+void DriverDownListen(){
+	int8_t Sup = -1;
+	int8_t Lup = -2;
+	while(1)
+	{
+		if(xSemaphoreGetMutexHolder(xStuckMutex) != xISRTASK){
+			xSemaphoreTake(xMovingSema,portMAX_DELAY);
+			int cnt = 0;
+			while(GPIOPinRead(GPIO_PORTB_BASE, GPIO_PIN_3) == GPIO_PIN_3){
+				//time_t secs = 1; // 2 minutes (can be retrieved from user's input)
+				//time_t startTime = time(NULL);
+				if(cnt != 0 && (GPIOPinRead(GPIO_PORTE_BASE, GPIO_PIN_5) == 0))
+					xQueueSendToBack(xWindowQueue, &Sup, 0);
+				cnt++;
+				//while (time(NULL) - startTime < secs);
+				if(cnt == 1)
+					for(int i = 0; i < 1000000; i++);
+			}
+			if(cnt == 1)
+				xQueueSendToBack(xWindowQueue, &Lup, 0);
+			xSemaphoreGive(xMovingSema);
+			taskYIELD();
+		}
+	}
+}
+
+void DriverUpListen(){
+	int8_t Sup = 1;
+	int8_t Lup = 2;
+	while(1)
+	{
+		if(xSemaphoreGetMutexHolder(xStuckMutex) != xISRTASK){
+			xSemaphoreTake(xMovingSema,portMAX_DELAY);
+			int cnt = 0;
+			while(GPIOPinRead(GPIO_PORTB_BASE, GPIO_PIN_2) == GPIO_PIN_2){
+				//time_t secs = 1; // 2 minutes (can be retrieved from user's input)
+				//time_t startTime = time(NULL);
+				if(cnt != 0 && (GPIOPinRead(GPIO_PORTE_BASE, GPIO_PIN_4) == 0))
+					xQueueSendToBack(xWindowQueue, &Sup, 0);
+				cnt++;
+				if(cnt == 1)
+					for(int i = 0; i < 1000000; i++);
+			}
+			if(cnt == 1)
+				xQueueSendToBack(xWindowQueue, &Lup, 0);
+			xSemaphoreGive(xMovingSema);
+			taskYIELD();
+		}
+	}
+}
+
+void PassDownListen(){
+	int8_t Sup = -1;
+	int8_t Lup = -2;
+	while(1)
+	{
+		if(xSemaphoreGetMutexHolder(xStuckMutex) != xISRTASK){
+			xSemaphoreTake(xMovingSema,portMAX_DELAY);
+			if(GPIOPinRead(GPIO_PORTD_BASE, GPIO_PIN_1) == 0){
+				int cnt = 0;
+				while(GPIOPinRead(GPIO_PORTD_BASE, GPIO_PIN_2) == GPIO_PIN_2){
+					//time_t secs = 1; // 2 minutes (can be retrieved from user's input)
+					//time_t startTime = time(NULL);
+					if(cnt != 0 && (GPIOPinRead(GPIO_PORTE_BASE, GPIO_PIN_5) == 0))
+						xQueueSendToBack(xWindowQueue, &Sup, 0);
+					cnt++;
+					//while (time(NULL) - startTime < secs);
+					if(cnt == 1)
+						for(int i = 0; i < 1000000; i++);
+				}
+				if(cnt == 1)
+					xQueueSendToBack(xWindowQueue, &Lup, 0);
+			}
+			xSemaphoreGive(xMovingSema);
+			taskYIELD();
+		}
+	}
+}
+
+void PassUpListen(){
+	int8_t Sup = 1;
+	int8_t Lup = 2;
+	while(1)
+	{
+		if(xSemaphoreGetMutexHolder(xStuckMutex) != xISRTASK){
+			xSemaphoreTake(xMovingSema,portMAX_DELAY);
+			if(GPIOPinRead(GPIO_PORTD_BASE, GPIO_PIN_1) == 0){
+				int cnt = 0;
+				while(GPIOPinRead(GPIO_PORTB_BASE, GPIO_PIN_1) == GPIO_PIN_1){
+					//time_t secs = 1; // 2 minutes (can be retrieved from user's input)
+					//time_t startTime = time(NULL);
+					if(cnt != 0 && (GPIOPinRead(GPIO_PORTE_BASE, GPIO_PIN_4) == 0))
+						xQueueSendToBack(xWindowQueue, &Sup, 0);
+					cnt++;
+					if(cnt == 1)
+						for(int i = 0; i < 1000000; i++);
+				}
+				if(cnt == 1)
+					xQueueSendToBack(xWindowQueue, &Lup, 0);
+			}
+			xSemaphoreGive(xMovingSema);
+			taskYIELD();
+		}
+	}
+}
+
+void WindowMove(){
+	int8_t data = 0;
+	//time_t secs;
+	//time_t startTime;
+	while(1){
+		xQueueReceive(xWindowQueue, &data, portMAX_DELAY);
+		switch(data){
+			case 1:
+				GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_2,GPIO_PIN_2);
+				GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3,0);
+				//secs = 1; // 2 minutes (can be retrieved from user's input)
+				//startTime = time(NULL);				
+				for(int i = 0; i < 1000000; i++);
+				GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_2,0);
+				break;
+			case 2:
+				GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_2,GPIO_PIN_2);
+				GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3,0);
+				xSemaphoreGive(xUpSema);
+				break;
+			case -1:
+				GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_2,0);
+				GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3,GPIO_PIN_3);
+				//secs = 1; // 2 minutes (can be retrieved from user's input)
+				//startTime = time(NULL);				
+				for(int i = 0; i < 1000000; i++);
+				GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3,0);
+				break;
+			case -2:
+				GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_2,0);
+				GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3,GPIO_PIN_3);
+				xSemaphoreGive(xDownSema);
+				break;
+		}
+	
+	}
 }
 
 
 int main(){
-
-
+	
+	xWindowQueue = xQueueCreate(5, sizeof(int8_t));
+	xMovingSema = xSemaphoreCreateMutex();
+	xUpSema = xSemaphoreCreateBinary();
+	xDownSema = xSemaphoreCreateBinary();
+	xEmptyQueueSema = xSemaphoreCreateBinary();
+	xStuckMutex = xSemaphoreCreateMutex();
+	xStuckSema = xSemaphoreCreateBinary();
+	
+	xSemaphoreGive(xMovingSema);
+	xSemaphoreGive(xStuckMutex);
+	
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+  while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOB));
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
+  while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOD));
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
+  while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOE));
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+  while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF));
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+  while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOA));
+	
+	GPIOPinTypeGPIOInput(GPIO_PORTB_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3);
+  GPIOPadConfigSet(GPIO_PORTB_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,GPIO_STRENGTH_2MA,GPIO_PIN_TYPE_STD_WPD);
+	GPIOPinTypeGPIOInput(GPIO_PORTD_BASE, GPIO_PIN_1|GPIO_PIN_2);
+  GPIOPadConfigSet(GPIO_PORTD_BASE, GPIO_PIN_1|GPIO_PIN_2,GPIO_STRENGTH_2MA,GPIO_PIN_TYPE_STD_WPD);
+	GPIOPinTypeGPIOInput(GPIO_PORTE_BASE, GPIO_PIN_4|GPIO_PIN_5);
+  GPIOPadConfigSet(GPIO_PORTE_BASE, GPIO_PIN_4|GPIO_PIN_5,GPIO_STRENGTH_2MA,GPIO_PIN_TYPE_STD_WPD);
+	GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2|GPIO_PIN_3);
+	
+	GPIOPinTypeGPIOInput(GPIO_PORTA_BASE, GPIO_PIN_2);
+  GPIOPadConfigSet(GPIO_PORTA_BASE, GPIO_PIN_2,GPIO_STRENGTH_2MA,GPIO_PIN_TYPE_STD_WPD);
+	GPIOIntDisable(GPIO_PORTA_BASE,GPIO_INT_PIN_2);
+  IntMasterEnable();
+  IntEnable(INT_GPIOA);
+  GPIOIntTypeSet(GPIO_PORTA_BASE,GPIO_PIN_2,GPIO_RISING_EDGE );
+	NVIC_SetPriority(mainSW_INTURRUPT_PortA,5);
+	GPIOIntEnable(GPIO_PORTA_BASE,GPIO_INT_PIN_2);
+	
+	
+	xTaskCreate(DriverUpListen,
+						"DriveruListen",
+						40,
+						NULL,
+						1,
+						NULL);
+	xTaskCreate(DriverDownListen,
+						"DriverdListen",
+						40,
+						NULL,
+						1,
+						NULL);
+	xTaskCreate(PassUpListen,
+						"PassengeruListen",
+						40,
+						NULL,
+						1,
+						NULL);
+	xTaskCreate(PassDownListen,
+						"PassengerdListen",
+						40,
+						NULL,
+						1,
+						NULL);
+	xTaskCreate(WindowMove,
+						"WindowMover",
+						40,
+						NULL,
+						2,
+						NULL);		
+	xTaskCreate(UpLimit,
+						"UpLimit",
+						40,
+						NULL,
+						1,
+						NULL);
+	xTaskCreate(DownLimit,
+						"DownLimit",
+						40,
+						NULL,
+						1,
+						NULL);
+	xTaskCreate(MSISR,
+						"ISRTASK",
+						40,
+						NULL,
+						5,
+						&xISRTASK);
 	vTaskStartScheduler();
+	
+	for( ;; ); 
+  
   return 0;	
 }
